d2fb75a507018f3e8b6087142b5800ce6e41946b
LucyKor <76470351+LucyKor@users.noreply.github.com>
2022-06-25 22:20:10 +0200

    Add files via upload

---
+++ Kalkulačka.py
@@ -0,0 +1,8 @@
+a = int(input("Zadej první hodnotu:"))
+b = int(input("Zadej druhou hodnotu:"))
+
+print("Součet je:", a+b)
+print("Rozdíl je:", a-b)
+print("Součin je:", a*b)
+print("Podíl je:", a/b)
+print("Výsledek:", a**b)
